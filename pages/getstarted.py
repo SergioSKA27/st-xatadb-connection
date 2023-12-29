eb21b65367948bed236f1bbfd1dae78641a1b671
SergioSKA27 <sergioska81@hotmail.com>
2023-12-29 03:21:16 -0600

    Add conclusion section to getstarted.py

--- pages/getstarted.py
+++ pages/getstarted.py
@@ -194,3 +194,20 @@ st.code("""
     "4b137344a3c53d5152c45ed514188cd2"
   ]
 }'""",language='json')
+
+
+st.markdown('''
+---
+## Conclusion
+
+The `st_xatadb_connection` package is a powerful tool that makes it easy to connect your Streamlit app to your Xata database.
+In this article, we've covered the basics of using `st_xatadb_connection` to query your Xata database, insert, update, and delete data,
+work with files, perform transactions, and ask questions to the AI assistant. But there's a lot more you can do with `st_xatadb_connection`.
+
+We encourage you to explore the `st_xatadb_connection` documentation on this site to learn more about the package's capabilities.
+We also encourage you to check out the `st_xatadb_connection` GitHub repository to see the source code and contribute to the project.
+
+<div style="text-align:center;font-family:monospace;font-size:18px">
+Thank you for reading this article!<br> We hope you found it useful and that it will help you build amazing data-driven web applications with Xata and Streamlit.
+</div>
+''',unsafe_allow_html=True)
