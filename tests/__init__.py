edef474f378950fa89dfd490ba758195c86a92bc
SergioSKA27 <sergioska81@hotmail.com>
2023-12-31 07:42:30 -0600

    Add test case for Xata database connection and query

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,3 +4,7 @@ import os
 at = AppTest.from_file("tests/test1.py")
 at.secrets["XATA_API_KEY"] = os.environ.get("XATA_API_KEY")
 at.secrets["XATA_API_SECRET"] = os.environ.get("XATA_DB_URL")
+at.run()
+
+# trunk-ignore(bandit/B101)
+assert not at.exception
